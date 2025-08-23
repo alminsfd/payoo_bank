48e50ed722342afa7cf6c99491be50d4a50e01c9
alminsfd <alaminhossaintanvir42@gmail.com>
2025-08-23 20:29:13 +0600

    add money popup genetator with funcnality

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 let btnLogin=document.getElementById("btn-login")
 btnLogin.addEventListener("click",function(event){
     event.preventDefault()
-    const mobileNumber=1863769996;
+    const mobileNumber=18637699996;
     const pinNumber=847986;
     let loginNumber=document.getElementById("number").value
     let loginPin=document.getElementById("pin").value
--- main.js
+++ main.js
@@ -1,8 +1,31 @@
+// Add money
+
 let addMoney=document.getElementById("add-money")
 let moneyPopup=document.getElementById("money-popup")
 let historyPopup=document.getElementById("history")
 addMoney.addEventListener("click",function(){
     moneyPopup.classList.remove("hidden")
     historyPopup.classList.add("hidden")
+
+})
+
+let addMoneybtn=document.getElementById("Mone-add")
+addMoneybtn.addEventListener("click",function(e){
+    e.preventDefault();
+    let bank=document.getElementById("bank").value 
+    let bankNumber=document.getElementById("acount-number").value
+    let moneyAdd=parseInt(document.getElementById("money-add").value)
+    let getpinNumber=parseInt(document.getElementById("pin").value)
+    let lastMoney=parseInt(document.getElementById("span").innerText) 
+    const pinNumber=847986;
+    if(bankNumber.length!=11){
+        alert("Invalid account number")
+        return
+    }
+    if(getpinNumber!==pinNumber){
+        alert("wrong pin")
+    }
+    let totalMoney=lastMoney+moneyAdd
+    document.getElementById("span").innerText=totalMoney
 
 })
